8a8ecd46cc3344a81fb2ddf8308b522e0cb4e8cb
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2020-07-06 20:43:45 -0500

    Pruebas sobre componentes de React

---
+++ 01-PrimerosPasos/counter-app/src/tests/PrimeraApp.test.js
@@ -0,0 +1,18 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render } from '@testing-library/react';
+import PrimeraApp from './../PrimeraApp';
+
+describe('Pruebas en <PrimeraApp />', () => {
+
+    test('Debe de mostrar el mensaje "Hola, soy Goku"', () => {
+
+        const saludo = 'Hola, soy Goku';
+
+        const { getByText } = render(<PrimeraApp saludo={ saludo } />);
+
+        expect(getByText(saludo)).toBeInTheDocument();
+
+    });
+
+});
