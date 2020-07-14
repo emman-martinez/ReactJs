375644dfed92aea2e0fb23cc7b76a05e45393ac1
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-07-13 20:49:09 -0500

    Pruebas del componente - GifGridItem

--- 06-GifExpertApp/gif-expert-app/src/tests/components/GifGridItem.test.js
+++ 06-GifExpertApp/gif-expert-app/src/tests/components/GifGridItem.test.js
@@ -7,16 +7,35 @@ describe('Pruebas en <GifGridItem />', () => {
 
     const title = 'Un título';
     const url = 'https://localhost/algo.jpg';
-
-    let wrapper = shallow( <GifGridItem title={title} url={url}/> );
-
-    beforeEach(() => {
-        wrapper = shallow( <GifGridItem title={title} url={url}/> );
-    });
+    const wrapper = shallow( <GifGridItem title={ title } url={ url } /> );
 
     test('Debe de mostrar el componente correctamente', () => {
 
         expect(wrapper).toMatchSnapshot();
+
+    });
+
+    test('Debe de tener un párrafo con el title', () => {
+
+        const h2 = wrapper.find('h2');
+        expect( h2.text().trim() ).toBe(title);  
+
+    });
+
+    test('Deber de tener la imagen igual al url y alt de las props', () => {
+
+        const img = wrapper.find('img');
+        // console.log(img.props());
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    });
+
+    test('Debe de tener animate_fadeIn', () => {
+
+        const div = wrapper.find('div');
+        // console.log(div.prop('className').includes('animate__fadeIn') ? 'animate__fadeIn' : '');
+        const className = div.prop('className').includes('animate__fadeIn');
+        expect(className).toBe(true); 
 
     });
 
