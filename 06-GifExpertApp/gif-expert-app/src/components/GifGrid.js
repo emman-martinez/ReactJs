b6885d0c11b502e4149409d9607a35aee044630d
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2020-07-15 13:50:20 -0500

    Pruebas del Componente GifGrid - Mock customHook

--- 06-GifExpertApp/gif-expert-app/src/components/GifGrid.js
+++ 06-GifExpertApp/gif-expert-app/src/components/GifGrid.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import useFetchGifs from '../hooks/useFetchGifs';
 import Chase from './../spinners/Chase';
 import GifGridItem from './GifGridItem';
@@ -30,5 +31,9 @@ const GifGrid = ({category}) => {
         </>
     )
 };
+
+GifGrid.propTypes = {
+    category: PropTypes.string.isRequired,
+}
 
 export default GifGrid;
---
+++ 06-GifExpertApp/gif-expert-app/src/tests/components/GifGrid.test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { shallow } from 'enzyme';
+import GifGrid from './../../components/GifGrid';
+import useFetchGifs from '../../hooks/useFetchGifs';
+jest.mock('../../hooks/useFetchGifs');
+
+describe('Puebas en <GifGrid />', () => {
+
+    const category = 'Goku';
+
+    test('Debe de mostrar el componente correctamente', () => {
+
+        useFetchGifs.mockReturnValue({
+            data: [],
+            loading: true,
+        });
+        
+        const wrapper = shallow( <GifGrid category={category}/> );
+        expect(wrapper).toMatchSnapshot();
+
+    });
+
+    test('Debe de mostrar items cuando se cargan imágenes useFetchGifs', () => {
+
+        const gifs = [
+            {
+                id: 'ABC',
+                url: 'https://localhost/cualquiercosa/cosa.jpg',
+                title: 'Cualquier cosa',
+            },
+            {
+                id: '123',
+                url: 'https://localhost/cualquiercosa/cosa.jpg',
+                title: 'Cualquier cosa',
+            }
+        ];
+
+        useFetchGifs.mockReturnValue({
+            data: gifs,
+            loading: false,
+        });
+        
+        const wrapper = shallow( <GifGrid category={category}/> );
+        const ChaseComponent = wrapper.find('Chase').exists();
+        const GifGridItemComponent = wrapper.find('GifGridItem');
+
+        expect(wrapper).toMatchSnapshot();
+        expect(ChaseComponent).toBe(false);
+        expect(GifGridItemComponent.length).toBe(gifs.length);
+
+    });
+
+});
