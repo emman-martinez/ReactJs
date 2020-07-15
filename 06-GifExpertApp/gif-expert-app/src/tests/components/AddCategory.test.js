8db205386946ec4cd219a41e4be4e350f8657aa0
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-07-14 22:47:16 -0500

    Simular Cambios del Input

--- 06-GifExpertApp/gif-expert-app/src/tests/components/AddCategory.test.js
+++ 06-GifExpertApp/gif-expert-app/src/tests/components/AddCategory.test.js
@@ -6,11 +6,21 @@ import AddCategory from './../../components/AddCategory';
 describe('Puebas en <AddCategory />', () => {
 
     const setCategories = () => {};
+    const wrapper = shallow( <AddCategory setCategories={setCategories}/> );
 
     test('Debe de mostrarse correctamente', () => {
-        const wrapper = shallow( <AddCategory setCategories={setCategories}/> );
-
         expect(wrapper).toMatchSnapshot();
+    });
+
+    test('Debe de cambiar la caja de texto', () => {
+        const input = wrapper.find('input');
+        const value = 'Hola Mundo';
+        input.simulate('change', {
+            target: {
+                value
+            }
+        });
+        expect(wrapper.find('p').text().trim()).toBe(value);
     });
 
 });
