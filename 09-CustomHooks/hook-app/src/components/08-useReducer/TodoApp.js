e7d9d35355ed7daa2b261966ee860bac3691b959
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-07-23 09:14:59 -0500

    Creando cascaron de la lista de TODOs

--- 09-CustomHooks/hook-app/src/components/08-useReducer/TodoApp.js
+++ 09-CustomHooks/hook-app/src/components/08-useReducer/TodoApp.js
@@ -2,11 +2,11 @@ import React, { useReducer } from 'react';
 import { todoReducer } from './todoReducer';
 import './styles.css';
 
-const initialState = {
+const initialState = [{
     id: new Date().getTime(),
     desc: 'Aprender React',
     done: false,
-};
+}];
 
 const TodoApp = () => {
 
@@ -15,13 +15,43 @@ const TodoApp = () => {
 
     return (
         <div className="container">
-            <h1>TodoApp</h1>
+            <h1>TodoApp ({todos.length}) </h1>
             <hr/>
-            <ul>
-                <li>Hola</li>
-                <li>Mundo</li>
-                <li>Hola de nuevo</li>
-            </ul>
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group list-group-flush">
+                        {
+                            todos.map((todo, i) => {
+                                return <li
+                                            key={todo.id}
+                                            className="list-group-item"
+                                        >
+                                            <p className="text-center"> {i + 1}. {todo.desc}</p>
+                                            <button className="btn btn-danger">Borrar</button>
+                                        </li>
+                            })
+                        }
+                    </ul>
+                </div>
+                <div className="col-5">
+                    <h4>Agregar TODO</h4>
+                    <hr/>
+                    <form>
+                        <input 
+                            type="text"
+                            name="description"
+                            className="form-control"
+                            placeholder="Aprender ..."
+                            autoComplete="off"
+                        />
+                        <button
+                            className="btn btn-primary mt-2 btn-block"
+                        >
+                            Agregar
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
