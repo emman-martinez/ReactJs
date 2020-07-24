1cf530459f7e934c293c452b9fecf30d99c04512
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2020-07-24 13:49:44 -0500

    Link y NavLink

--- 09-CustomHooks/hook-app/src/components/09-useContext/AppRouter.js
+++ 09-CustomHooks/hook-app/src/components/09-useContext/AppRouter.js
@@ -2,8 +2,10 @@ import React from 'react';
 import {
     BrowserRouter as Router,
     Switch,
-    Route
+    Route,
+    Redirect
 } from "react-router-dom";
+import NavBar from './NavBar';
 import HomeScreen from './HomeScreen';
 import AboutScreen from './AboutScreen';
 import LoginScreen from './LoginScreen';
@@ -12,10 +14,12 @@ const AppRouter = () => {
     return (
         <Router>
             <div>
+                <NavBar />
                 <Switch>
                     <Route exact path="/" component={ HomeScreen }/>
                     <Route exact path="/about" component={ AboutScreen }/>
                     <Route exact path="/login" component={ LoginScreen }/>
+                    <Redirect to="/" />
                 </Switch>
             </div>
         </Router>
