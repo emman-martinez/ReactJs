b067aef39889a25d8a7ee6ca87c14eda577f97ce
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-07-26 13:47:11 -0500

    Pruebas sobre useFetch - CustomHook

--- 09-CustomHooks/hook-app/src/hooks/useFetch.js
+++ 09-CustomHooks/hook-app/src/hooks/useFetch.js
@@ -38,6 +38,13 @@ const useFetch = (url) => {
                     });
                 }
 
+            })
+            .catch(() => {
+                setState({
+                    data: null,
+                    loading: false,
+                    error: 'No se pudo cargar la info'
+                });
             });
 
     }, [url]);
