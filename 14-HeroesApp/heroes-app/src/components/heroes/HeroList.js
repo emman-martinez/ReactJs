d7953761e8fb352b1ab4e1748d583b63e8b9e1cd
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-08-01 11:26:59 -0500

    Animaciones en nuestro componente

--- 14-HeroesApp/heroes-app/src/components/heroes/HeroList.js
+++ 14-HeroesApp/heroes-app/src/components/heroes/HeroList.js
@@ -9,7 +9,7 @@ const HeroList = ({ publisher }) => {
     //  const heroes = getHeroesByPublisher(publisher);
 
     return (
-        <div className="card-columns">
+        <div className="card-columns animate__animated animate__fadeIn">
             {
                 heroes.map(hero => (
                     <HeroCard 
