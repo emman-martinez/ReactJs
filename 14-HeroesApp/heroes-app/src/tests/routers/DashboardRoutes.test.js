d9d6c7add776fb7770645e43da605b4b1cbe5412
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2020-08-05 09:48:32 -0500

    Pruebas en el componente DashboardRoutes

---
+++ 14-HeroesApp/heroes-app/src/tests/routers/DashboardRoutes.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { DashboardRoutes } from '../../routers/DashboardRoutes';
+import { AuthContext } from '../../auth/AuthContext';
+import '@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Pruebas en <DashboardRoutes />', () => {
+
+    const contextValue = {
+        dispatch: jest.fn(),
+        user: {
+            logged: true,
+            name: 'Casandra',
+        }
+    };
+
+    test('Debe de mostrarse correctamente', () => {
+        
+        const wrapper = mount( 
+            <AuthContext.Provider value={ contextValue }>
+                <MemoryRouter>
+                    <DashboardRoutes />
+                </MemoryRouter>
+            </AuthContext.Provider>
+        );
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('.text-info').text().trim()).toBe('Casandra');
+
+    });
+    
+});
