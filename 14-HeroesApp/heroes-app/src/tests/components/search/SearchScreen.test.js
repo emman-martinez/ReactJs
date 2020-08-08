8e6e6965777a08777d6b26f32c0d75aebffb18cf
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-08-08 12:36:39 -0500

    Pruebas en el SearchScreen

---
+++ 14-HeroesApp/heroes-app/src/tests/components/search/SearchScreen.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { mount } from 'enzyme';
+import { MemoryRouter, Route } from 'react-router-dom';
+import SearchScreen from '../../../components/search/SearchScreen';
+
+describe('Pruebas en <SearchScreen />', () => {
+
+    test('Debe de mostrarse correctamente con valores por defecto', () => {
+        
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search']}>
+                <Route path="/search" component={ SearchScreen } />
+            </MemoryRouter>
+        );
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('.alert-info').text().trim()).toBe('Search a hero...')
+
+    });
+
+    test('Debe de Mostrar a Batman y el input con el valor del queryString', () => {
+        
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search?q=batman']}>
+                <Route path="/search" component={ SearchScreen } />
+            </MemoryRouter>
+        );
+
+        expect(wrapper.find('input').prop('value')).toBe('batman');
+        expect(wrapper).toMatchSnapshot();
+
+    });
+    
+    
+});
