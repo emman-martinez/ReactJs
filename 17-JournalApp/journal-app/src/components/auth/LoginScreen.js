eb0043647af432a220d723a3c7c9111f1f2cbcae
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-08-11 10:43:30 -0500

    Estilos LoginScreen Pt2

--- 17-JournalApp/journal-app/src/components/auth/LoginScreen.js
+++ 17-JournalApp/journal-app/src/components/auth/LoginScreen.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const LoginScreen = () => {
     return (
         <div>
-            <h3>Login</h3>
+            <h3 className="auth__title">Login</h3>
             <form>
                 <input 
                     type="type"
                     placeholder="Email..."
                     name="email"
+                    className="auth__input"
+                    autoComplete="off"
                 />
                 <input 
                     type="password"
                     placeholder="Password..."
                     name="password"
+                    className="auth__input"
                 />
                 <button
                     type="submit"
@@ -22,7 +26,23 @@ export const LoginScreen = () => {
                 </button>
 
                 <hr/>
-                <p>google</p>
+                
+                <div>
+                    <p>Login with social networks</p>
+                    <div className="google-btn">
+                        <div className="google-icon-wrapper">
+                            <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+                        </div>
+                        <p className="btn-text">
+                            <b>Sign in with google</b>
+                        </p>
+                    </div>
+                </div>
+
+                <Link to="/auth/register">
+                    Create New Account
+                </Link>
+
             </form>
         </div>
     );
