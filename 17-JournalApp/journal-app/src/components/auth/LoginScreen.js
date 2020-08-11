e48620a95882a9dec106c84080d23bbe2d41f829
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-08-11 09:53:46 -0500

    Sass Partials & Estilos del AuthRouter

--- 17-JournalApp/journal-app/src/components/auth/LoginScreen.js
+++ 17-JournalApp/journal-app/src/components/auth/LoginScreen.js
@@ -3,7 +3,27 @@ import React from 'react';
 export const LoginScreen = () => {
     return (
         <div>
-            <h1>LoginScreen</h1>
+            <h3>Login</h3>
+            <form>
+                <input 
+                    type="type"
+                    placeholder="Email..."
+                    name="email"
+                />
+                <input 
+                    type="password"
+                    placeholder="Password..."
+                    name="password"
+                />
+                <button
+                    type="submit"
+                >
+                    Login
+                </button>
+
+                <hr/>
+                <p>google</p>
+            </form>
         </div>
     );
 };
