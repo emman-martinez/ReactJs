a377b358406320c8f094dea236563f9c2e6bb551
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2020-08-14 17:13:03 -0500

    NoteAppBar

---
+++ 17-JournalApp/journal-app/src/components/notes/NoteScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NotesAppBar } from './NotesAppBar';
+
+export const NoteScreen = () => {
+    return (
+        <div>
+            <div className="notes__main-content">
+
+                <NotesAppBar />
+
+            </div>          
+        </div>
+    );
+};
