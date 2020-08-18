36c668a39bf33584842e4a1f2f5dfe498a6b23b2
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2020-08-17 21:32:12 -0500

    uiReducer y Acciones

--- 17-JournalApp/journal-app/src/components/auth/RegisterScreen.js
+++ 17-JournalApp/journal-app/src/components/auth/RegisterScreen.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setError, removeError } from '../../redux/actions/ui';
 import validator from 'validator';
 import useForm from '../../hooks/useForm';
 
 export const RegisterScreen = () => {
+
+    const dispatch = useDispatch();
 
     const initialForm = {
         name: 'Emmanuel',
@@ -26,18 +30,18 @@ export const RegisterScreen = () => {
     };
 
     const isFormValid = () => {
-        
+
         if(name.trim().length === 0) {
-            console.log('Name is required');
+            dispatch(setError('Name is required'));
             return false;
         } else if (!validator.isEmail(email)) {
-            console.log('Email is not valid');
+            dispatch(setError('Email is not valid'));
             return false;
         } else if (password !== password2 || password.length < 5) {
-            console.log('Password should be at least 6 characters and match each other');
+            dispatch(setError('Password should be at least 6 characters and match each other'));
             return false;
         }
-
+        dispatch(removeError());
         return true;
     };
 
---
+++ 17-JournalApp/journal-app/src/redux/actions/ui.js
@@ -0,0 +1,18 @@
+import { types } from "../types/types";
+
+export const setError = (err) => {
+
+    return {
+        type: types.uiSetError,
+        payload: err,
+    };
+
+};
+
+export const removeError = (err) => {
+
+    return {
+        type: types.uiRemoveError
+    };
+
+};
