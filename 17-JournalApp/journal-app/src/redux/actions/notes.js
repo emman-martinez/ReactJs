91e69999ee4f203bdf5b1921cafb93f9fa1dbd97
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-08-20 09:40:31 -0500

    Activar la nota creada

--- 17-JournalApp/journal-app/src/redux/actions/notes.js
+++ 17-JournalApp/journal-app/src/redux/actions/notes.js
@@ -1,4 +1,5 @@
 import { db } from "../../firebase/firebase-config";
+import { types } from "../types/types";
 
 export const startNewNote = () => {
 
@@ -16,7 +17,20 @@ export const startNewNote = () => {
         const doc = await db.collection(`${uid}/journal/notes`).add(newNote);
 
         console.log(doc);
+        dispatch(activeNote(doc.id, newNote));
 
+    };
+
+};
+
+export const activeNote = (id, note) => {
+
+    return {
+        type: types.notesActive,
+        payload: {
+            id,
+            ...note
+        }
     };
 
 };
--- 17-JournalApp/journal-app/src/redux/reducers/notesReducer.js
+++ 17-JournalApp/journal-app/src/redux/reducers/notesReducer.js
@@ -20,7 +20,14 @@ const initialState = {
 
 export const notesReducer = (state = initialState, action) => {
 
-    switch(action.type) {
+    switch (action.type) {
+        case types.notesActive:
+            return {
+                ...state,
+                active: {
+                    ...action.payload
+                }
+            }
         default:
             return state;
     }
