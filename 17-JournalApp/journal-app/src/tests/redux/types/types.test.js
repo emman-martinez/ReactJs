967c6b0c26d62de1099630fe603f6c6bf69950fe
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2020-08-26 17:28:53 -0500

    JournalApp Testing

---
+++ 17-JournalApp/journal-app/src/tests/redux/types/types.test.js
@@ -0,0 +1,30 @@
+import '@testing-library/jest-dom';
+import { types } from '../../../redux/types/types';
+
+describe('Pruebas con nuestros types', () => {
+
+    test('Debe de tener estos types', () => {
+
+        expect(types).toEqual({
+
+            login: '[Auth] Login',
+            logout: '[Auth] Logout',
+        
+            uiSetError: '[UI] Set Error',
+            uiRemoveError: '[UI] Remove Error',
+        
+            uiStartLoading: '[UI] Start loading',
+            uiFinishLoading: '[UI] Finish loading',
+        
+            notesAddNew: '[Notes] New note',
+            notesActive: '[Notes] Set active note',
+            notesLoad: '[Notes] Load notes',
+            notesUpdated: '[Notes] Updated note',
+            notesFileUrl: '[Notes] Updated image url',
+            notesDelete: '[Notes] Delete note',
+            notesLogoutCleaning: '[Notes] Logout Cleaning',
+        
+        });
+    });
+
+});
