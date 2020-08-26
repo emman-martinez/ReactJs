e2d332bc2eae60651542e3eaa25e9e6b3ffd72d3
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2020-08-26 15:58:45 -0500

    Animaciones en nuestra aplicación

--- 17-JournalApp/journal-app/src/components/journal/JournalScreen.js
+++ 17-JournalApp/journal-app/src/components/journal/JournalScreen.js
@@ -10,7 +10,7 @@ export const JournalScreen = () => {
     const { active } = state;
 
     return (
-        <div className="journal__main-content">
+        <div className="journal__main-content animate__animated animate__fadeIn animate__faster">
             <Sidebar />
             <main>
                 {
