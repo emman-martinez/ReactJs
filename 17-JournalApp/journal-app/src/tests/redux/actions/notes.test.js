2583af31c398e653593addabc4dbde6f4141bb3b
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-08-28 10:47:13 -0500

    Puebas en acciones de notes pt1

---
+++ 17-JournalApp/journal-app/src/tests/redux/actions/notes.test.js
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom';
+import { types } from '../../../redux/types/types';
+
+describe('Pruebas en notes-actions', () => {
+
+    test('Todas las acciones deben de funcionar', () => {
+
+
+    });
+
+});
