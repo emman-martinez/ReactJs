1576854923b23055d092f17a492a876ce02c2bb0
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-09-01 21:32:53 -0500

    Pruebas en startLoadingNotes y startSaveNote

--- 17-JournalApp/journal-app/src/tests/redux/actions/notes.test.js
+++ 17-JournalApp/journal-app/src/tests/redux/actions/notes.test.js
@@ -1,20 +1,27 @@
 import '@testing-library/jest-dom';
 import configureStore from 'redux-mock-store'; //ES6 modules
 import thunk from 'redux-thunk';
-import { startNewNote } from '../../../redux/actions/notes';
+import { startNewNote, startLoadingNotes, startSaveNote } from '../../../redux/actions/notes';
 import { types } from '../../../redux/types/types';
 import { db } from '../../../firebase/firebase-config';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
-
-const store = mockStore({
+const initState = {
     auth: {
         uid: 'TESTING',
     }
-});
+};
+
+let store = mockStore(initState);
 
 describe('Pruebas con las acciones de notes', () => {
+
+    beforeEach(() => {
+
+        store = mockStore(initState);
+
+    });
 
     test('Debe de crear una nueva nota startNewNote', async() => {
 
@@ -49,5 +56,51 @@ describe('Pruebas con las acciones de notes', () => {
         await db.doc(`/TESTING/journal/notes/${docId}`).delete();
 
     });
+
+    test('startLoadingNotes debe cargar la notas ', async() => {
+
+        await store.dispatch(startLoadingNotes('TESTING'));
+
+        const actions = store.getActions();
+        //console.log(actions);
+
+        expect(actions[0]).toEqual({
+            type: types.notesLoad,
+            payload: expect.any(Array),
+        });
+
+        const expected = {
+            id: expect.any(String),
+            title: expect.any(String),
+            body: expect.any(String),
+            date: expect.any(Number),
+        };
+
+        expect(actions[0].payload[0]).toMatchObject(expected);
+
+    });
+
+    test('startSaveNote debe de actualizar la nota', async() => {
+
+        const note = {
+            id: 'LG6vMG1YVuOBMJdeJEMt',
+            title: 'titulo',
+            body: 'body'
+        };
+
+        await store.dispatch(startSaveNote(note));
+
+        const actions = store.getActions();
+        // console.log(actions);
+
+        expect(actions[0].type).toBe(types.notesUpdated);
+
+        const docRef = await db.doc(`TESTING/journal/notes/${note.id}`).get();
+
+        expect(docRef.data().title).toBe(note.title);
+
+    });
+
+
 
 });
