b05d4e85542d43dd1921effd91017537b197db6e
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-09-01 22:49:47 -0500

    Pruebas en LoginScreen

---
+++ 17-JournalApp/journal-app/src/tests/components/auth/LoginScreen.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store'; //ES6 modules
+import thunk from 'redux-thunk';
+import '@testing-library/jest-dom';
+import { LoginScreen } from '../../../components/auth/LoginScreen';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+const initState = {
+    auth: {},
+    ui: {
+        loading: false,
+        msgError: null,
+    }
+};
+
+let store = mockStore(initState);
+
+const wrapper = mount(
+        <Provider store={ store }>
+            <MemoryRouter>
+                <LoginScreen />
+            </MemoryRouter>
+        </Provider>
+    );
+
+describe('Pruebas en <LoginScreen />', () => {
+
+    beforeEach(() => {
+        store = mockStore(initState);
+    });
+
+    test('Debe de mostrarse correctamente', () => {
+        
+        expect(wrapper).toMatchSnapshot();
+        
+    });
+    
+});
