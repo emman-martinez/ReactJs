3d49b4879d7b222d8e06199994d0d974e5ad1083
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-09-03 11:35:07 -0500

    Pruebas en el RegisterScreen

---
+++ 17-JournalApp/journal-app/src/tests/components/auth/RegisterScreen.test.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store'; //ES6 modules
+import thunk from 'redux-thunk';
+import '@testing-library/jest-dom';
+import { RegisterScreen } from '../../../components/auth/RegisterScreen';
+import { types } from '../../../redux/types/types';
+
+// jest.mock('../../../redux/actions/auth', () => ({
+
+//     startGoogleLogin: jest.fn(),
+//     startLoginEmailPassword: jest.fn()
+
+// }));
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+const initState = {
+    auth: {},
+    ui: {
+        loading: false,
+        msgError: null,
+    }
+};
+
+let store = mockStore(initState);
+//  store.dispatch = jest.fn();
+
+const wrapper = mount(
+        <Provider store={ store }>
+            <MemoryRouter>
+                <RegisterScreen />
+            </MemoryRouter>
+        </Provider>
+    );
+
+describe('Pruebas en <RegisterScreen />', () => {
+
+    test('Debe de mostrarse correctamente', () => {
+
+        expect(wrapper).toMatchSnapshot();
+
+    });
+
+    test('Debe de hacer el dispatch de la acción respectiva', () => {
+        
+        const emailField = wrapper.find('input[name="email"]');
+        emailField.simulate('change', {
+            target: {
+                value: '',
+                name: 'email'
+            }
+        });
+
+        wrapper.find('form').simulate('submit', {
+            preventDefault(){}
+        });
+
+        const actions = store.getActions();
+        //  console.log(actions);
+
+        expect(actions[0]).toEqual({
+            type: types.uiSetError,
+            payload: 'Email is not valid'
+        });
+
+    });
+    
+
+});
